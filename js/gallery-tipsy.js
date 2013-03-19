48451eb2598a55fb88f1e0e081a827d7566a80fc
Tilo Mitra <tilomitra@gmail.com>
2013-03-19 17:38:59 -0400

    add showOn, hideOn ATTRS, add visibility toggling

--- js/gallery-tipsy.js
+++ js/gallery-tipsy.js
@@ -1,20 +1,21 @@
 
 
-var getCN = Y.ClassNameManager.getClassName,
+var Lang = Y.Lang,
+    getCN = Y.ClassNameManager.getClassName,
 
-//HTML5 Data Attributes
-DATA_CONTENT = 'data-content',
-DATA_PLACEMENT = 'data-placement',
+    //HTML5 Data Attributes
+    DATA_CONTENT = 'data-content',
+    DATA_PLACEMENT = 'data-placement',
 
-//Classes
-TIPSY = 'tipsy',
-FADE = 'fade',
-IN = 'in',
+    //Classes
+    TIPSY = 'tipsy',
+    FADE = 'fade',
+    IN = 'in',
 
-CLASSES = {
-    fade: getCN(TIPSY, FADE),
-    fadeIn: getCN(TIPSY, IN)
-};
+    CLASSES = {
+        fade: getCN(TIPSY, FADE),
+        fadeIn: getCN(TIPSY, IN)
+    };
 
 
 Y.Tipsy = Y.Base.create("tipsy", Y.Widget, [Y.WidgetPointer, Y.WidgetPosition, Y.WidgetPositionAlign, Y.WidgetStack], {
@@ -23,7 +24,7 @@ Y.Tipsy = Y.Base.create("tipsy", Y.Widget, [Y.WidgetPointer, Y.WidgetPosition, Y
 
     //constructor
     initializer : function(config) {
-        
+
     },
 
     //clean up on destruction
@@ -34,22 +35,55 @@ Y.Tipsy = Y.Base.create("tipsy", Y.Widget, [Y.WidgetPointer, Y.WidgetPosition, Y
     },
 
     renderUI : function () {
-        this.get('boundingBox').addClass(CLASSES.fade);
+        this.get('boundingBox').addClass(CLASSES.fade).setAttribute('role', 'tooltip');
     },
 
     bindUI : function () {
         var del = this.get('delegate'),
-            selector = this.get('selector');
+            selector = this.get('selector'),
+            showOn = this.get('showOn');
 
-        this._handles.push(del.delegate(['mouseover', 'touchstart'], this._handleDelegateStart, selector, this));
+        //showOn = ['event1', 'event2']
+        if (Lang.isArray(showOn) || Lang.isString(showOn)) {
+            this._handles.push(del.delegate(showOn, this._handleDelegateStart, selector, this));
+        }
+
+        //showOn = { events: ['event1', 'event2'] }
+        else if (Lang.isObject(showOn) && !showOn.node) {
+            this._handles.push(del.delegate(showOn.events, this._handleDelegateStart, selector, this));
+        }
+
+        //showOn = { node: '#selector', events: ['event1', 'event2'] }
+        else if (Lang.isObject(showOn) && showOn.node && showOn.events) {
+            this._handles.push(Y.one(showOn.selector).on(showOn.events, this._handleDelegateStart, this));
+        }
+        else {
+            Y.log('The showOn attribute should contain an array of events, or an object with keys "selector" (string), and "events" (array of events)');
+        }
     },
 
     _handleDelegateStart : function (e) {
         var del = this.get('delegate'),
             selector = this.get('selector'),
+            hideOn = this.get('hideOn'),
             node = e.currentTarget;
 
-        this._handles.push(del.delegate(['mouseout', 'touchend'], this._handleDelegateEnd, selector, this));
+        if (Lang.isArray(hideOn) || Lang.isString(hideOn)) {
+            this._handles.push(del.delegate(hideOn, this._handleDelegateEnd, selector, this));
+        }
+
+        //hideOn = { events: ['event1', 'event2'] }
+        else if (Lang.isObject(hideOn) && !hideOn.selector) {
+            this._handles.push(del.delegate(hideOn.events, this._handleDelegateEnd, selector, this));
+        }
+
+        //hideOn = { node: '#selector', events: ['event1', 'event2'] }
+        else if (Lang.isObject(hideOn) && hideOn.selector && hideOn.events) {
+            this._handles.push(Y.one(hideOn.selector).on(hideOn.events, this._handleDelegateEnd, this));
+        }
+        else {
+            Y.log('The hideOn attribute should contain an array of events, or an object with keys "selector" (string), and "events" (array of events)');
+        }
 
         this.showTooltip(node);
     },
@@ -62,22 +96,34 @@ Y.Tipsy = Y.Base.create("tipsy", Y.Widget, [Y.WidgetPointer, Y.WidgetPosition, Y
         this._setTooltipContent(node);
         this._alignTooltip(node);
         this.alignPointer(node);
-        this.get('boundingBox').addClass(CLASSES.fadeIn);
+        this._showTooltip();
+        this.get('boundingBox').addClass(CLASSES.fadeIn).setAttribute('aria-hidden', 'false');
+        node.setAttribute('aria-describedby', this.get('boundingBox').getAttribute('id'));
     },
     
+    _showTooltip: function () {
+        this.set('visible', true);
+    },
+
     hideTooltip : function () {
-        this.get('boundingBox').removeClass(CLASSES.fadeIn);
+        this.get('boundingBox').removeClass(CLASSES.fadeIn).setAttribute('aria-hidden', 'true');
+        this._hideTooltip();
     },
     
+    _hideTooltip: function () {
+        this.set('visible', false);
+    },
+
     _setTooltipContent: function (node) {
-        var content = node.getAttribute(DATA_CONTENT) || this.get('content'),
+
+        var content = (node.hasAttribute(DATA_CONTENT)) ? node.getAttribute(DATA_CONTENT) : this.get('content'),
             contentBox = this.get('contentBox');
 
         contentBox.setContent(content);
     },
     
     _alignTooltip : function (node) {
-        var placement = node.getAttribute(DATA_PLACEMENT) || this.get('placement');
+        var placement = (node.hasAttribute(DATA_PLACEMENT)) ? node.getAttribute(DATA_PLACEMENT) : this.get('placement');
 
         switch (placement) {
             case "above":
@@ -111,6 +157,14 @@ Y.Tipsy = Y.Base.create("tipsy", Y.Widget, [Y.WidgetPointer, Y.WidgetPosition, Y
 
         zIndex: {
             value: 2
+        },
+
+        showOn: {
+            value: ['mouseover', 'touchstart', 'focus']
+        },
+
+        hideOn: {
+            value: ['mouseout', 'touchend', 'blur']
         },
         
         delegate: {
